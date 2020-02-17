d98e474ccee6c062e17fb84edcaabed830fce424
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-17 10:35:32 +0200

    Add files via upload

---
+++ JavaCore 0807 LinkedList and ArrayList/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+/*
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.util.*;
+
+
+public class Main {
+    public static Object createArrayList() {
+        ArrayList<Object> list = new ArrayList<>();
+        return list;
+    }
+    public static Object createLinkedList() {
+        LinkedList<Object> lList = new LinkedList<>();
+        return lList;
+    }
+    public static void main(String[] args) throws Exception {
+
+    }
+}
+
+
+
+
+
